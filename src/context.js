bc507030958e7a894e4eef055aaaf45247d843f4
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-06-16 14:04:28 +0600

    context setup

--- src/context.js
+++ src/context.js
@@ -15,7 +15,29 @@ const tempUrl = 'https://opentdb.com/api.php?amount=10&category=21&difficulty=me
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+
+  const [waiting, setWaiting] = useState(true);
+  const [loading,setLoading] = useState(false);
+  const [questions,setQuestions] = useState([]);
+  const [index,setIndex] = useState(0);
+  const [correct,setCorrect] = useState(0);
+  const [error,setError] = useState(false);
+
+  const [isModalOpen,setModalOpen] = useState(false);
+
+  return <AppContext.Provider 
+  value=
+  {{
+    waiting,
+    loading,
+    questions,
+    index,
+    correct,
+    error,
+    isModalOpen
+    }}>
+    {children}
+    </AppContext.Provider>
 }
 // make sure use
 export const useGlobalContext = () => {
